cd5f386e7ba33e25759e60b1ad8bdf15f9b0d5d8
EvheniySlobodyanik <exiletopchik@gmail.com>
2026-05-20 17:52:21 +0300

    Implement recipe_detail view to display specific recipe by id

--- recipe/views.py
+++ recipe/views.py
@@ -4,3 +4,7 @@ from .models import Recipe
 def main(request):
     recipes = Recipe.objects.filter(created_at__year=2023)
     return render(request, 'recipe/main.html', {'recipes': recipes})
+
+def recipe_detail(request, id):
+    recipe = get_object_or_404(Recipe, id=id)
+    return render(request, 'recipe/recipe_detail.html', {'recipe': recipe})    
